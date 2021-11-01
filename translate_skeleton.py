fca2f0bf74658040babe8cb5a66623ede464add1
ak2485 <ak2485@cornell.edu>
2021-11-01 13:17:24 -0400

    start of state class

--- translate_skeleton.py
+++ translate_skeleton.py
@@ -7,6 +7,36 @@ print("parsing " + HARMONY_FILE)
 with open(HARMONY_FILE) as f:
     har_json = json.load(f)
 
+class Context():
+    def __init__(self):
+        # create a context
+        self.stack = []
+        self.pc = []
+        self.vars = {}
+
+class HState():
+    def __init__(self):
+        # create an initial Harmony state
+        # see https://harmony.cs.cornell.edu/site/reference/language/harmony-vm/
+        #self.code = TODO
+        #self.labels = TODO
+
+        """a dictionary mapping variable names to values; starts empty"""
+        self.variables = {}
+
+        """a dictionary that maps contexts to values; starts with init context
+           values represent number of equivalent contexts in state """
+        cxt1        = Context()
+        self.ctxbag = {cxt1: 1}
+
+        #self.stopbag = TODO
+        #self.choosing = TODO
+
+    def updateState(instr):
+        if instr == "Push"
+
+
+
 def parse_line(line):
     if line['op'] == 'Frame':
         return ""
@@ -16,8 +46,11 @@ def parse_line(line):
         pass
 
 def main():
+    state = HState()
     for line in har_json['code']:
-        print(parse_line(line))
+        instr = line['op']
+        updateState(instr)
+        #print(parse_line(line))
 
 if __name__ == "__main__":
     main()
